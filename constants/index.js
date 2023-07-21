83b29139917347a35f4196ad8f375d4e6befb727
GITPINATA <140170947+GITPINATA@users.noreply.github.com>
2023-07-21 14:28:51 +0200

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x779c7aecdbec5ab53a60ef7948bee8207c4dd251";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
